91b0f40f1e2b3173fc8c44021e7926a1e2dbc498
Timofey Konshin <tkonshin@astralinux.ru>
2025-12-05 14:58:59 +0000

    Clear code

--- source/megalaycontroller.h
+++ source/megalaycontroller.h
@@ -1,47 +1,6 @@
-//------------------------------------------------------------------------
-// Project     : VST SDK
-// Version     : 3.1.0
-//
-// Category    : Examples
-// Filename    : public.sdk/samples/vst/again/source/againcontroller.h
-// Created by  : Steinberg, 04/2005
-// Description : AGain Editor Example for VST 3.0
-//
-//-----------------------------------------------------------------------------
-// LICENSE
-// (c) 2010, Steinberg Media Technologies GmbH, All Rights Reserved
-//-----------------------------------------------------------------------------
-// This Software Development Kit may not be distributed in parts or its entirety  
-// without prior written agreement by Steinberg Media Technologies GmbH. 
-// This SDK must not be used to re-engineer or manipulate any technology used  
-// in any Steinberg or Third-party application or software module, 
-// unless permitted by law.
-// Neither the name of the Steinberg Media Technologies nor the names of its
-// contributors may be used to endorse or promote products derived from this 
-// software without specific prior written permission.
-// 
-// THIS SDK IS PROVIDED BY STEINBERG MEDIA TECHNOLOGIES GMBH "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL STEINBERG MEDIA TECHNOLOGIES GMBH BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
-//-----------------------------------------------------------------------------
-
 #pragma once
 
 #include "public.sdk/source/vst/vsteditcontroller.h"
-
-#define READ_PARAMETER(SavedParameter) \
-	int32 SavedParameter = 0.f;\
-	if (state->read (&SavedParameter, sizeof (int32)) != kResultOk)\
-	{\
-		return kResultFalse;\
-	}
 
 namespace Steinberg {
 namespace Vst {
@@ -66,46 +25,27 @@ public:
 	}
 
 	//---from IPluginBase--------
-	tresult PLUGIN_API initialize (FUnknown* context);
-	tresult PLUGIN_API terminate  ();
+    tresult PLUGIN_API initialize (FUnknown* context) override;
+    tresult PLUGIN_API terminate  () override;
 
 	//---from EditController-----
-	tresult PLUGIN_API setComponentState (IBStream* state);
-	//IPlugView* PLUGIN_API createView (const char* name);
-	tresult PLUGIN_API setState (IBStream* state);
-	tresult PLUGIN_API getState (IBStream* state);
-	tresult PLUGIN_API setParamNormalized (ParamID tag, ParamValue value);
-	tresult PLUGIN_API getParamStringByValue (ParamID tag, ParamValue valueNormalized, String128 string);
-	tresult PLUGIN_API getParamValueByString (ParamID tag, TChar* string, ParamValue& valueNormalized);
-	//void editorDestroyed (AMegalayEditorView* editor) {} // nothing to do here
-	//void editorAttached (AMegalayEditorView* editor);
-	//void editorRemoved (AMegalayEditorView* editor);
-
-	//int32 PLUGIN_API getProgramListCount ();
-	//tresult PLUGIN_API getProgramListInfo (int32 listIndex, ProgramListInfo& info /*out*/);
-	//tresult PLUGIN_API getProgramName (ProgramListID listId, int32 programIndex, String128 name /*out*/);
-	//tresult PLUGIN_API getProgramInfo (ProgramListID listId, int32 programIndex, CString attributeId, String128 attributeValue);
+    tresult PLUGIN_API setComponentState (IBStream* state) override;
+    tresult PLUGIN_API setState (IBStream* state) override;
+    tresult PLUGIN_API getState (IBStream* state) override;
+    tresult PLUGIN_API setParamNormalized (ParamID tag, ParamValue value) override;
+    tresult PLUGIN_API getParamStringByValue (ParamID tag, ParamValue valueNormalized, String128 string) override;
+    tresult PLUGIN_API getParamValueByString (ParamID tag, TChar* string, ParamValue& valueNormalized) override;
 
 	//---from ComponentBase-----
-	tresult receiveText (const char* text);
+    tresult receiveText (const char* text) override;
 
 	//---from IMidiMapping-----------------
-	tresult PLUGIN_API getMidiControllerAssignment (int32 busIndex, int16 channel, CtrlNumber midiControllerNumber, ParamID& tag);
+    tresult PLUGIN_API getMidiControllerAssignment (int32 busIndex, int16 channel, CtrlNumber midiControllerNumber, ParamID& tag) override;
 
 	DELEGATE_REFCOUNT (EditController)
-	tresult PLUGIN_API queryInterface (const char* iid, void** obj);
-
-	//---Internal functions-------
-	//void addDependentView (AMegalayEditorView* view);
-	//void removeDependentView (AMegalayEditorView* view);
-
-	//void setDefaultMessageText (String128 text);
-	//TChar* getDefaultMessageText ();
-//------------------------------------------------------------------------
+    tresult PLUGIN_API queryInterface (const char* iid, void** obj) override;
 
 private:
-	//TArray <AMegalayEditorView*> viewsArray;
-	//String128 defaultMessageText;
 	int32 iMidiGain;
 	int32 iMidiAuxGain;
 	int32 iMidiDelay;
@@ -118,7 +58,6 @@ private:
 	int32 iMidiPower;
 	int32 iMidiPingPong;
 	int32 iMidiBypass;
-
 };
 
  }} // namespaces
